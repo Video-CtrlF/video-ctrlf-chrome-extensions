30de13bcbfd449124ae1d17822bffca31d6c79d5
dev-loggi <jsl663663@gmail.com>
2023-07-31 00:03:22 +0900

    fix: 유튜브 홈페이지의 SPA 방식으로 인한 이슈 해결
    - SPA 방식으로 유튜브 동영상 재생 페이지에서 content.js 실행이 안되는 문제 발생
    - content_scripts > matches 속성을 유튜브 전체로 변경
    - 페이지 변화 감지를 위해 MutationObserver를 활용하여 문제 해결

--- script/content.js
+++ script/content.js
@@ -26,31 +26,100 @@ const CONTENT_CAPTION_SAMPLE_PATH = "sample/caption-sample.json"
 const CONTENT_HTML_PATH = 'html/content.html'
 const CONTENT_CSS_PATH = 'css/content.css'
 
-// 유튜브 비디오 엘리먼트
-const videoElement = document.querySelector("video.video-stream")
+const YTD_WATCH_FLEXY_SECONDARY = 'ytd-watch-flexy[flexy] #secondary.ytd-watch-flexy'
+const VIDEO_SELECTOR = '#movie_player > div.html5-video-container > video'
+
+// 유튜브 홈페이지가 SPA(Single Page Application) 방식인 문제를 해결하기 위한 변수
+// 유튜브 홈페이지 메인()에서 동영상 재생 페이지로 넘어갔을때를 MutationObserver와 url 변화로 감지함
+let lastUrl = ''
 
 let youtubeCaptions = []
 let sttCaptions = []
 
-// 검색 유형 라디오 옵션
-let radioOptions
+async function showCtrlfContainer() {
+    console.log('showCtrlfContainer()')
 
-async function loadData() {
-    console.log('loadData()')
+    await generateCtrlfContainer()
+
+    const scripts = await requestVideoScripts()
+
+    await showCtrlfScripts(scripts)
+}
+
+function removeCtrlfContainer() {
+    console.log('removeCtrlfContainer()')
+    const container = document.getElementById('ctrlf')
+    if (container != null) {
+        container.parentElement.removeChild(container)
+    }
+}
+
+async function generateCtrlfContainer() {
+    console.log('generateCtrlfContainer()')
 
     const html = await getHtml()
     const css = await getCss()
+
+    // 유튜브 화면에서 오른쪽에 배치 된 컨텐츠
+    const secondary = document.querySelector(YTD_WATCH_FLEXY_SECONDARY)
+    console.log('secondary=' + secondary)
+
+    // 'secondary' sideBar 의 첫 번째 자식 요소로 삽입
+    secondary.insertAdjacentHTML('afterbegin', html)
+
+    const rootContainer = secondary.firstElementChild
+    const scrollView = document.getElementById('ctrlf-scroll-view')
+
+    // Collapsible 버튼 이벤트
+    const collapsibleButton = document.querySelector('.ctrlf-collapsible-button')
+    collapsibleButton.addEventListener('click', toggleContent)
+
+    // 검색 유형 라디오 버튼
+    const radioOptions = document.querySelectorAll('.ctrlf-search-type-radio')
+    radioOptions.forEach((radioOption) => {
+        radioOption.addEventListener('click', () => {
+            const selectedOption = document.querySelector('.ctrlf-search-type-radio:checked')
+            console.log("onClick(): " + selectedOption.value)
+            if (selectedOption) {
+                if (selectedOption.value === 'stt') {
+                    console.log("selectedOption.value === 'stt'")
+                } else if (selectedOption.value === 'ocr') {
+                    console.log("selectedOption.value === 'ocr'")
+                }
+            }
+        })
+    })
+}
+
+function toggleContent() {
+    // 토글 아이콘 회전
+    const collapsibleIcon = document.querySelector('.ctrlf-collapsible-icon')
+    collapsibleIcon.classList.toggle('active')
+
+    // 컨텐츠 펼치기/접기
+    const content = document.querySelector('.ctrlf-content')
+    console.log('content: ' + content)
+    console.log('content.style.maxHeight: ' + content.style.maxHeight)
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px"
+    }
+}
+
+async function requestVideoScripts() {
+    console.log('requestVideoScripts()')
+
     // const jsonObject = await getData()
     const jsonObject = await getSampleData()
 
-    if (html != null && css != null && jsonObject != null) {
+    if (jsonObject != null) {
         youtubeCaptions = jsonObject.caption.map(item =>
             new Caption(item.url_id, item.start_time, item.end_time, item.text)
         )
-        showData(html, css, youtubeCaptions)
-    } else {
-        console.log(html, css, jsonObject)
     }
+
+    return youtubeCaptions
 }
 
 async function getHtml() {
@@ -74,68 +143,20 @@ async function getSampleData() {
         .then(response => response.json())
 }
 
-function showData(html, css, captions) {
-    console.log("showData()")
-    console.log(captions)
+function showCtrlfScripts(scripts) {
+    console.log('showCtrlfScripts()')
+    console.log(scripts)
 
-    // 유튜브 화면에서 오른쪽에 배치 된 컨텐츠
-    const rightContents = document.getElementById('secondary')
-
-    // 'secondary' sideBar 의 첫 번째 자식 요소로 삽입
-    rightContents.insertAdjacentHTML('afterbegin', html)
-    const rootContainer = rightContents.firstElementChild
     const scrollView = document.getElementById('ctrlf-scroll-view')
-
-    // Collapsible 버튼 이벤트
-    const collapsibleButton = document.querySelector('.ctrlf-collapsible-button')
-    collapsibleButton.addEventListener('click', toggleContent)
+    scrollView.innerHTML = ''
 
     // 목록 만들기
-    for (let i = 0; i < captions.length; i++) {
-        const caption = captions[i]
+    for (let i = 0; i < scripts.length; i++) {
+        const caption = scripts[i]
         const captionElement = createCaptionItemElement(caption)
 
         scrollView.append(captionElement)
     }
-
-    // 검색어 인풋
-    const searchInput = document.getElementById("ctrlf-search-input")
-    searchInput.addEventListener("input", function () {
-        const keyword = searchInput.value.trim().toLowerCase()
-
-        let filteredCaptions
-        if (keyword.length === 0) {
-            filteredCaptions = youtubeCaptions
-        } else {
-            filteredCaptions = youtubeCaptions.filter(caption => {
-                return caption.text.toLowerCase().includes(keyword)
-            })
-        }
-
-        scrollView.innerHTML = ""
-        filteredCaptions.forEach(caption => {
-            const captionElement = createCaptionItemElement(caption)
-            scrollView.appendChild(captionElement)
-        })
-    })
-
-    // 검색 유형 라디오 버튼
-    radioOptions = document.querySelectorAll('.ctrlf-search-type-radio')
-    radioOptions.forEach((radioOption) => {
-        radioOption.addEventListener('click', () => {
-            const selectedOption = document.querySelector('.ctrlf-search-type-radio:checked')
-            console.log("onClick(): " + selectedOption.value)
-            if (selectedOption) {
-                if (selectedOption.value === 'stt') {
-                    console.log("selectedOption.value === 'stt'")
-                } else if (selectedOption.value === 'ocr') {
-                    console.log("selectedOption.value === 'ocr'")
-                }
-            }
-        })
-    })
-
-
 }
 
 function createCaptionItemElement(caption) {
@@ -162,7 +183,9 @@ function createCaptionItemElement(caption) {
 
     // 클릭 이벤트
     row.addEventListener("click", function () {
-        videoElement.currentTime = caption.start_time
+        const video = document.querySelector(VIDEO_SELECTOR)
+        console.log('video=' + video)
+        video.currentTime = caption.start_time
     })
 
     row.append(timeDiv)
@@ -171,27 +194,22 @@ function createCaptionItemElement(caption) {
     return row
 }
 
-function toggleContent() {
-    // 토글 아이콘 회전
-    const collapsibleIcon = document.querySelector('.ctrlf-collapsible-icon')
-    collapsibleIcon.classList.toggle('active')
-
-    // 컨텐츠 펼치기/접기
-    const content = document.querySelector('.ctrlf-content')
-    console.log('content: ' + content)
-    console.log('content.style.maxHeight: ' + content.style.maxHeight)
-    if (content.style.maxHeight){
-        content.style.maxHeight = null
-    } else {
-        content.style.maxHeight = content.scrollHeight + "px"
-    }
-}
-
 function logError(error) {
     console.log(error)
 }
 
-setTimeout(loadData, 2000) // 1초 뒤에 실행
-// document.addEventListener('DOMContentLoaded', loadData)
+const observer = new MutationObserver((mutations) => {
+    if (lastUrl !== location.href) {
+        lastUrl = location.href
+        console.log('lastUrl=' + lastUrl)
+
+        if (lastUrl.includes('https://www.youtube.com/watch')) {
+            showCtrlfContainer()
+        } else {
+            removeCtrlfContainer()
+        }
+    }
+})
+observer.observe(document.getElementById('page-manager'), { childList: true, subtree: true })
 
 console.log('content.js :: Bottom!!')
